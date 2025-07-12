f9cc2c407f8d2fd978713c1a9f97889778f4b531
Iftikhar Afridi <iftikharafridi@gmail.com>
2025-07-12 15:51:54 +0100

    initial commit

---
+++ index.mjs
@@ -0,0 +1,29 @@
+import express from 'express';
+import Database from 'better-sqlite3';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import path from 'path';
+
+const app = express();
+const db = new Database('./database.db', {verbose: console.log});
+
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json())
+
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) =>{
+    console.log("Welcome to discoverhealth app");
+    //res.send("Welcome to discoverhealth app");
+    res.render('index.html')
+})
+
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
